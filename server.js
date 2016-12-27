b7cb982cca19892a1ff49f3ae9126964600c715f
nutnon <nuttapol.nonsee@gmail.com>
2016-12-27 22:37:47 +0000

    Let Heroku set port.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express =   require(`express`)
 var moment	=	require(`moment`)
 var app = express()
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
     res.send(`Timestamp API`)
 })
@@ -17,9 +21,9 @@ app.get('/:time', function(req, res) {
     }
     
     // Send output.
-    res.send( JSON.stringify(outpTime))
+    res.send( JSON.stringify(outpTime));
 })
 
-app.listen(8080, function() {
+app.listen(port, function() {
     console.log('Example app listen on port 8080!')
 })
